8982cfa0a7801d862c1978e7ad0b312ebfbc40f2
wookie <lawanski@gmail.com>
2025-07-13 13:40:01 +0200

    Task-3: Endpoint for showing invoice

--- src/Modules/Invoices/Presentation/routes.php
+++ src/Modules/Invoices/Presentation/routes.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Route;
 use Modules\Invoices\Presentation\Http\CreateInvoiceController;
 use Modules\Invoices\Presentation\Http\SendInvoiceController;
+use Modules\Invoices\Presentation\Http\ShowInvoiceController;
 
 Route::post('invoices', CreateInvoiceController::class)->name('invoices.create');
 Route::post('invoices/{invoiceId}/send', SendInvoiceController::class)->name('invoices.send');
+
+Route::get('invoices/{invoiceId}', ShowInvoiceController::class)->name('invoices.show');
